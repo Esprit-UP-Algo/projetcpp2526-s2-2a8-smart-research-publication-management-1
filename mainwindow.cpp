af6599f1327debbf9768c8014a4bdf146966791a
oussema05-dev <Oussema.Benjemaa@esprit.tn>
2026-02-08 01:06:13 +0100

    commit again

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,34 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QApplication>
-#include <QLocale>
-#include <QTranslator>
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
-
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "nouveauqt_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    MainWindow w;
-    w.show();
-    return a.exec();
 }
